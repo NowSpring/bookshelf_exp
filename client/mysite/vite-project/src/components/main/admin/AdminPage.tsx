a8398c1627534af8dbc265dd795efd7cbd87a1d9
NowSpring <kodai218@icloud.com>
2024-06-08 02:51:30 +0900

    add download function

--- client/mysite/vite-project/src/components/main/admin/AdminPage.tsx
+++ client/mysite/vite-project/src/components/main/admin/AdminPage.tsx
@@ -3,6 +3,8 @@ import { useLocation } from 'react-router-dom';
 import Books from "./Books";
 import { useEffect, useState } from 'react';
 import { BookListType } from '../types';
+import { Button } from '@/components/ui/button';
+import { FileDown } from "lucide-react";
 
 const AdminPage = () => {
 
@@ -27,12 +29,28 @@ const AdminPage = () => {
     getBookLists();
   }, [bookListType]);
 
+  const downloadJsonFile = () => {
+    const dataStr = JSON.stringify(allBookLists, null, 2);
+    const blob = new Blob([dataStr], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = `${bookListType.type}_book_lists.json`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
   return (
     <div>
-      <p style={{ fontWeight: 'bold', fontSize: '24px' }}>
-        「{ bookListType.type }」の推し棚
-      </p>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <p style={{ fontWeight: 'bold', fontSize: '24px', marginRight: '10px' }}>
+          「{ bookListType.type }」の推し棚
+        </p>
+        <Button size="icon" onClick={downloadJsonFile} className="rounded-md">
+          <FileDown />
+        </Button>
+      </div>
       <div
         style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
       >
